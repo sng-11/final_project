a8ec9ff13dd2e7b5abb54b8cef261ab4a4be11ae
melissarlogan <85718354+melissarlogan@users.noreply.github.com>
2021-11-16 20:48:37 -0500

    Update Inner_join_code.sql

--- postgres_files/Inner_join_code.sql
+++ postgres_files/Inner_join_code.sql
@@ -1,4 +1,4 @@
--- Creating tables for PH-EmployeeDB
+-- Creating tables for final demographics
 CREATE TABLE final_demographics (
 	user_id VARCHAR(40) NOT NULL,
 	gender VARCHAR(40) NOT NULL,
@@ -12,6 +12,22 @@ CREATE TABLE plays_sum (
 	user_id VARCHAR(40) NOT NULL,
 	minutes_viewed INT NOT NULL,
 	ML_Output INT NULL,
+PRIMARY KEY (user_id)
+);
+
+-- Creating tables for users
+CREATE TABLE users (
+	user_id VARCHAR(40) NOT NULL,
+	country_code VARCHAR(5) NOT NULL,
+PRIMARY KEY (user_id)
+);
+
+-- Creating tables for characteristics
+CREATE TABLE characteristics (
+	index VARCHAR(40) NOT NULL,
+	user_id VARCHAR(40) NOT NULL,
+	platform VARCHAR(40) NOT NULL,
+	characteristics VARCHAR(100) NOT NULL,
 PRIMARY KEY (user_id)
 );
 
@@ -36,6 +52,22 @@ ml_model1.age,
 ml_model1.minutes_viewed,
 users.country_code,
 ml_model1.ml_output
+INTO ml_model2
 FROM ml_model1
 INNER JOIN users
 ON ml_model1.user_id = users.user_id;
+
+-- joining ml_model2 with characteristics
+
+SELECT ml_model2.user_id,
+ml_model2.gender,
+ml_model2.income_level,
+ml_model2.age,
+ml_model2.minutes_viewed,
+ml_model2.country_code,
+characteristics.platform,
+characteristics.characteristics
+ml_model2.ml_output
+FROM ml_model2
+INNER JOIN characteristics
+ON ml_model2.user_id = characteristics.user_id;
