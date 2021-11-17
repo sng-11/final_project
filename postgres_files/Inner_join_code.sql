7659fcaf3764c8e4ed3e6149c0fabf72c59089e4
melissarlogan <85718354+melissarlogan@users.noreply.github.com>
2021-11-16 20:26:51 -0500

    Update Inner_join_code.sql

--- postgres_files/Inner_join_code.sql
+++ postgres_files/Inner_join_code.sql
@@ -22,6 +22,20 @@ final_demographics.income_level,
 final_demographics.age,
 plays_sum.minutes_viewed,
 plays_sum.ml_output
+INTO ml_model1
 FROM final_demographics
 INNER JOIN plays_sum
 ON final_demographics.user_id = plays_sum.user_id;
+
+-- joining ml_model1 with users
+
+SELECT ml_model1.user_id,
+ml_model1.gender,
+ml_model1.income_level,
+ml_model1.age,
+ml_model1.minutes_viewed,
+users.country_code,
+ml_model1.ml_output
+FROM ml_model1
+INNER JOIN users
+ON ml_model1.user_id = users.user_id;
